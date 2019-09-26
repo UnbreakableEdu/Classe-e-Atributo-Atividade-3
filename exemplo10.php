0ecc00ce98b468c51a9f9d633be0bab42be79b8b
Eduardo Onofre <48128069+UnbreakableEdu@users.noreply.github.com>
2019-09-26 08:36:03 -0300

    Atividade Orientação Objeto 5 - __construct

---
+++ exemplo10.php
@@ -0,0 +1,21 @@
+<?php
+
+class Usuario
+{
+// as propriedades
+private $primeiroNome;
+private $ultimoNome;
+// metodo que diz Ola ao usuario
+public function __construct($primeiroNome,$ultimoNome)
+{
+$this->primeiroNome=$primeiroNome; 
+$this->ultimoNome=$ultimoNome;
+}
+public function getNomeCompleto(){
+    return "Olá, " . $this->primeiroNome." ".$this->ultimoNome.", seja bem-vindo";
+    }
+}
+
+$user1 = new Usuario("Johnny","Bravo");
+
+echo $user1->getNomeCompleto();
